ed823cb91042a5339d30f63e081665132005ef31
Brenna Meyer <brennameyer9@gmail.com>
2025-04-27 11:10:05 -0500

    Add project files.

---
+++ Chapter 7 Exercise 12 - Part 1.cpp
@@ -0,0 +1,23 @@
+// Chapter 7 Exercise 12 - Part 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Programmer:Brenna Meyer
+// Date 4/27/2025
+//Grade Book
+//A teacher has five students who have taken four tests.The teacher uses the following grading scale to assign a letter grade to a student, based on the average of his or her four test scores :
+//Test Score
+//Letter Grade
+//90–100 A
+//80–89 B
+//70–79 C
+//60–69 D
+//0–50 F
+
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
